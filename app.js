c491c264add0874880cb48edf32174aa2f727ada
Leon <liujy@xbed.com.cn>
2016-05-16 15:26:34 +0800

    完成教程：第1章--一个简单的博客

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo/es5')(session);
 
 // 加载路由控制
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+var settings = require('./settings');
+
+var flash = require('connect-flash');
+// var users = require('./routes/users');
 
 // 创建项目实例
 var app = express();
@@ -27,39 +33,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // 定义cookie解析器
 app.use(cookieParser());
+// 配置SESSION
+app.use(session({
+    secret: settings.cookieSecret,
+    key: settings.db, //cookie name
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }, //30 days
+    store: new MongoStore({
+        url: 'mongodb://localhost/blog'
+    }),
+    resave: false,
+    saveUninitialized: true
+}));
+// 使用 flash 功能
+app.use(flash());
 // 定义静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
-
 // 匹配路径和路由
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
 
 // 404错误处理
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // 开发环境，500错误处理和错误堆栈跟踪
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // 生产环境，500错误处理
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 // 输出模型app
 module.exports = app;
